52c1793e9db0b14edb59c21f1c7da112e5d982c0
tycrek <gpg+github@jmoore.dev>
2023-01-23 12:19:56 -0700

    feat: added frontend stuff

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+	separator: '_',
+	content: ['./**/*.pug'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'space-mono': ['Space Mono', 'monospace'],
+				lato: ['Lato', 'sans-serif']
+			},
+		}
+	}
+};
