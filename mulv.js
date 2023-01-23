242dcb13c042b25ce1914d5c23ece8a4070eaa81
Josh Moore <github.com@jmoore.dev>
2023-01-23 09:55:01 -0700

    Update mulv.js

--- mulv.js
+++ mulv.js
@@ -29,3 +29,11 @@ const getSkin = (username) => new Promise((resolve, reject) =>
 		.then((imageResponse) => /*sharp(Buffer.from(imageResponse.data, 'base64'))*/ imageResponse.data)
 		.then(resolve)
 		.catch(reject));
+
+export default function handler(request, response) {
+	response.status(200).json({
+		body: request.body,
+		query: request.query,
+		cookies: request.cookies,
+	});
+}
